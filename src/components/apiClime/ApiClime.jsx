90fed995b225596fb395b1e689f74e7cbf86b1f6
Jose Gregorio Perez Montilla <solintperez@gmail.com>
2023-08-21 14:09:13 -0400

    agragando app clima para pruebas

--- src/components/apiClime/ApiClime.jsx
+++ src/components/apiClime/ApiClime.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import ApiClime from './components/apiClime/ApiClime'
 
 function App() {
 
   const [pokemon,setPokemon] = useState();
   const [nombrePokemon, setNombrePokemon] = useState('');
 
-  const obtenerPokemon = () => {axios.get('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon)
+  const obtenerPokemon = () => {axios.get('https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={e920d87ad8a741b2e9c693a7d1e336a7}')
       .then(function (response) {
         console.log(response.data);
         setPokemon(response.data);
@@ -27,7 +26,6 @@ function App() {
     <button onClick={obtenerPokemon}>Buscar</button>
     { pokemon &&
       <>
-        <ApiClime></ApiClime>
         <h1>{pokemon.name}</h1>
         <h2>{pokemon.id}</h2>
         <img src={pokemon.sprites.front_shiny}></img>
